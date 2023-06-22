bc6627a06e290fcb459baae23b77aee382be904c
Eunji <ej.lim@hicomp.net>
2023-06-22 16:53:49 +0900

    230622

--- publish/js/sign.js
+++ publish/js/sign.js
@@ -89,3 +89,13 @@ function phoneCheck (checkNum) {
     }
 }
 
+// modal
+function modal_close(){
+    document.getElementById('modal_wrap').style.display = "none";
+}
+function modal_open(content){
+    document.getElementById('modal_wrap').style.display = "block";
+    let modal = document.getElementById('modal');
+    let modalContent = modal.childNodes;
+}
+
