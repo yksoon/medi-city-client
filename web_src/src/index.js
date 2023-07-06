830b256ed57bed99160dbbb2cb812ab703d3d72a
용광순 <gs.yong@hicomp.net>
2023-07-06 17:03:45 +0900

    인증테스트7

--- web_src/src/index.js
+++ web_src/src/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "redux/store/store";
 import { persistStore } from "redux-persist";
