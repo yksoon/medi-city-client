a288333f814cb5f30053b683e952c68bfb6a9c54
용광순 <gs.yong@hicomp.net>
2023-08-12 20:02:03 +0900

    lazy

--- web_src/src/Router.js
+++ web_src/src/Router.js
@@ -33,6 +33,7 @@ const Router = () => {
                         color: "#fff",
                         zIndex: (theme) => theme.zIndex.drawer + 1,
                     }}
+                    open={true}
                 >
                     <CircularProgress color="inherit" />
                 </Backdrop>
