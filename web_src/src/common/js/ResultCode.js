fef46eaebcb950137be777c167250f1bb22603dd
용광순 <gs.yong@hicomp.net>
2023-09-08 09:15:34 +0900

    디렉토리 삭제

--- web_src/src/common/js/ResultCode.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-
-const ResultCode = () => {
-    let resultCode = useSelector((state) => state.codes.resultCode);
-
-    return resultCode;
-};
-
-export { ResultCode };
